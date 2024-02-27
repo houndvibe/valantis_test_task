ffaa669a4c0f5b067d549823f465631618aba933
Artem Lipatov <houndvibe@gmail.com>
2024-02-27 22:07:26 +0600

    api done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
+import { useEffect } from "react";
+import ValantisApi from "./api/valantisApi";
+
+const x = async () => {
+  const response1 = await ValantisApi.get_ids(0, 50);
+  console.log(response1);
+  const response2 = await ValantisApi.get_items(response1);
+  console.log(response2);
+  const response3 = await ValantisApi.get_fields("brand", 3, 100);
+  console.log(response3);
+};
+
 const App = () => {
+  useEffect(() => {
+    x();
+  }, []);
+
   return <>App</>;
 };
 
--- src/api/valantisApi.ts
+++ src/api/valantisApi.ts
@@ -1,19 +1,72 @@
 import $axios_auth from "./interceptor";
 
 export interface ValantisActionParams {
+  price?: number;
+  offset?: number;
+  limit?: number;
+  ids?: string[];
+  field?: string;
+}
+
+type ProductBrand = null | string;
+
+type MasterQueryResponse =
+  | Promise<string[]>
+  | Promise<ProductProps>
+  | Promise<ProductBrand[]>;
+
+export interface ProductProps {
+  brand: ProductBrand;
+  id: string;
   price: number;
-  offset: number;
-  limit: number;
-  ids: string[];
-  field: string;
+  product: string;
 }
 
 export default class ValantisApi {
-  static async sendQuery(action: string, params: ValantisActionParams) {
+  static async sendMasterQuery(
+    action: string,
+    params: ValantisActionParams
+  ): Promise<MasterQueryResponse> {
     const response = await $axios_auth.post("/", {
       action,
       params,
     });
-    return response;
+    return response.data.result;
+  }
+
+  static async get_ids(offset: number, limit: number): Promise<string[]> {
+    const response = await $axios_auth.post("/", {
+      action: "get_ids",
+      params: { offset, limit },
+    });
+    return response.data.result;
+  }
+
+  static async get_items(ids: string[]): Promise<ProductProps> {
+    const response = await $axios_auth.post("/", {
+      action: "get_items",
+      params: { ids },
+    });
+    return response.data.result;
+  }
+
+  static async get_fields(
+    field: string,
+    offset: number,
+    limit: number
+  ): Promise<ProductBrand[]> {
+    const response = await $axios_auth.post("/", {
+      action: "get_fields",
+      params: { field, offset, limit },
+    });
+    return response.data.result;
+  }
+
+  static async filter(params: ValantisActionParams): Promise<string[]> {
+    const response = await $axios_auth.post("/", {
+      action: "filter",
+      params,
+    });
+    return response.data.result;
   }
 }
