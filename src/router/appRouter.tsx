6cae4be1e429c949012421bc1fe696a757ff63cb
Artem Lipatov <houndvibe@gmail.com>
2024-03-04 21:13:38 +0500

    deploy env settings

--- src/router/appRouter.tsx
+++ src/router/appRouter.tsx
@@ -4,31 +4,36 @@ import App from "../pages/App/App";
 import InfoPage from "../pages/InfoPage";
 import ProductsPage from "../pages/ProductsPage/ProductsPage";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <>Что то пошло не так...</>,
-    children: [
-      {
-        index: true,
-        element: (
-          <Suspense>
-            <InfoPage />
-          </Suspense>
-        ),
-        errorElement: <>Что то пошло не так...</>,
-      },
-      {
-        path: "products",
-        element: (
-          <Suspense>
-            <ProductsPage />
-          </Suspense>
-        ),
-        errorElement: <>Что то пошло не так...</>,
-      },
-    ],
-  },
-]);
+console.log(import.meta.env.BASE_URL);
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <>Что то пошло не так...</>,
+      children: [
+        {
+          index: true,
+          element: (
+            <Suspense>
+              <InfoPage />
+            </Suspense>
+          ),
+          errorElement: <>Что то пошло не так...</>,
+        },
+        {
+          path: "products",
+          element: (
+            <Suspense>
+              <ProductsPage />
+            </Suspense>
+          ),
+          errorElement: <>Что то пошло не так...</>,
+        },
+      ],
+    },
+  ],
+  { basename: import.meta.env.BASE_URL }
+);
 export default router;
